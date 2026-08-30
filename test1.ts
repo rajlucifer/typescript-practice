a43fc0b517572829a4a438647c44094c8c2ed778
rahul raj <thisisrajtkd18@gmail.com>
2026-08-30 18:10:31 +0530

    typescript practice session

---
+++ test1.ts
@@ -0,0 +1,109 @@
+// const a: number;
+// a=12;
+// console.log(a);
+
+// this will show error because in const must provide the value during initialized time then it work
+
+// know it work
+const a:number =12;
+console.log(a);
+
+// this string 
+let username:string ;
+username="hello";
+console.log(username)
+
+
+// this is boolean
+let isLoggedIn:boolean;
+isLoggedIn= true;
+console.log(isLoggedIn);
+
+
+// making array datatype is number
+
+let array:number[];
+// array=[1,2,3,"hello"];// in this we decleared the array in the number form thatswhy thatswhy string show error
+
+array=[1,7,8];
+console.log(array);
+
+// making array datatype is string
+
+let arr:string[];
+// one more way to assign the array in ts
+let arr2 :Array<string>;
+
+console.log(arr=["hello","world"]);
+
+// enum
+// it used to defind the particular role which can not be change
+// enum role {
+//     "admin","user"
+// }
+
+//any is the default type in the typescript
+// in real world we mainly don't use the any in project
+
+// let value;
+// value = "hello"
+// value=12,
+// value=true;
+
+let value:any;
+value = "hello"
+value=12,
+value=true;
+
+
+function adding(a:number,b:number){
+
+    const sum = a+b;
+    return sum;
+
+}
+
+const result = adding(2,5);
+console.log(result);
+
+
+//type inference  means typescript automatically know the datatype easily  when we decleared value  below code showing no error
+
+let num1=33;
+
+// type declearation 
+let num2:number =77;
+
+
+//type aliase it is use to make the coustom type scipt
+
+type id= number| string ; //now here we make the custom type 
+
+let myId:id ="hello"; // no error is comming
+
+// best use of type
+type status = "success" | "error" | "pending"
+
+let b:status;
+
+b="success";
+
+
+
+// how to make the object  one more thing we have to write all info about the fields that are present otherwise show error like 
+// if we don't write the age then show the error
+
+
+type user ={
+    name:string,
+    age:number
+};
+
+let obj:user;
+obj={
+    name:"kira",
+    age:21
+
+}
+
+console.log(obj);
