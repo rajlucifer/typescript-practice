80d32559c4a7e8e18f9814c8ca4642c99dfc3c18
rahul raj <thisisrajtkd18@gmail.com>
2026-09-12 18:06:54 +0530

    creating  middleware using nextjs tsc

--- test1.ts
+++ test1.ts
@@ -251,6 +251,7 @@ type childrenType={
 // this we call the reactnode
 export default function RootLayout({children} : childrenType )
 
+////////////////////////////////////////////////////////////
 
 // proxy ts
 import { NextResponse } from 'next/server'
@@ -264,6 +265,21 @@ export function proxy(request: NextRequest) {
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
  
+export const config = {
+  matcher: '/about/:path*',
+}
+
+
+//middleware 
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
 export const config = {
   matcher: '/about/:path*',
 }
